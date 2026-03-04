dfe6424f69d6f30df33b4a593f39eff5271fb7f5
LidiaYavorova <lidiq098@abv.bg>
2026-03-04 17:55:25 +0200

    feat: set up basic routing for the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Battle from "./pages/Battle/Battle";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/battle" element={<Battle />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
